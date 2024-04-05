06cd4b2ba438238db52ad568b7eadc7d192c69bb
CALEB KOMEN <komencaleb502@gmail.com>
2024-04-05 08:17:47 +0300

    map case

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+  const titleCased = () => {
+    return tutorials.map(title => {
+      return title.split(' ').map(word => {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      }).join(' ');
+    });
+  };
+  
+  module.exports = { titleCased };
+
+
